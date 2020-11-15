c5890652e1e4a923f53dac3c045a4f75d08dc0c7
Anno von Heimburg <anno.vonheimburg@40three.io>
2020-11-15 09:30:02 +0100

    Change default keys to left and right bracket

--- DVStokerMod/Settings.cs
+++ DVStokerMod/Settings.cs
@@ -8,13 +8,13 @@ namespace DVStokerMod
         [Draw("Cycle Stoker Up", DrawType.KeyBinding)]
         public KeyBinding StokerUp = new KeyBinding()
         {
-            keyCode = KeyCode.Quote
+            keyCode = KeyCode.RightBracket
         };
 
         [Draw("Cycle Stoker Down", DrawType.KeyBinding)]
         public KeyBinding StokerDown = new KeyBinding()
         {
-            keyCode = KeyCode.Semicolon
+            keyCode = KeyCode.LeftBracket
         };
         public void OnChange() { }
         
